d7a97016bcc58b2cd5731d8b89c80f057439cc56
Michael Botelho <saggiatoreblue@gmail.com>
2020-11-10 22:06:05 -0500

    DFS

--- src/com/michaelbotelho/Main.java
+++ src/com/michaelbotelho/Main.java
@@ -260,4 +260,33 @@ public class Main {
         }
     }
 
+    /*
+    Depth First Search
+
+    Solution # 1 O(v+e) time | O(v) space
+    */
+    class DFS {
+        public class Node {
+            String name;
+            List<Node> children = new ArrayList<Node>();
+
+            public Node(String name) {
+                this.name = name;
+            }
+
+            public List<String> depthFirstSearch(List<String> array) {
+                array.add(this.name);
+                for (int i = 0; i < children.size(); i++) {
+                    children.get(i).depthFirstSearch(array);
+                }
+                return array;
+            }
+
+            public Node addChild(String name) {
+                Node child = new Node(name);
+                children.add(child);
+                return this;
+            }
+        }
+    }
 }
