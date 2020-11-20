dd59e4062c0b847d05be6379745e11240c997635
Michael Botelho <saggiatoreblue@gmail.com>
2020-11-20 11:22:46 -0500

    Product sum

--- src/com/michaelbotelho/Main.java
+++ src/com/michaelbotelho/Main.java
@@ -332,4 +332,26 @@ public class Main {
     private ArrayList<Integer> reverseArray(ArrayList<Integer> array) {
        return array;
     }
+
+    /*
+      Product Sum
+
+      Solution # 1 O(n) time | O(d) space d is equal to greatest depth
+      */
+    public static int productSum(List<Object> array) {
+        return productSumHelper(array, 1);
+    }
+    public static int productSumHelper(List<Object> array, int multiplier) {
+        int sum = 0;
+        for (Object el : array) {
+            if (el instanceof ArrayList) {
+                @SuppressWarnings("unchecked")
+                ArrayList<Object> ls = (ArrayList<Object>) el;
+                sum += productSumHelper(ls, multiplier + 1);
+            } else {
+                sum += (int) el;
+            }
+        }
+        return sum * multiplier;
+    }
 }
