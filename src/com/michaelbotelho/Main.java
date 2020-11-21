e10907731c603c90f6ab325ba9f9adc5e75c3322
Michael Botelho <saggiatoreblue@gmail.com>
2020-11-21 17:36:00 -0500

    Find largest numbers

--- src/com/michaelbotelho/Main.java
+++ src/com/michaelbotelho/Main.java
@@ -5,9 +5,8 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        int[] arr = new int[] {0, 1, 21, 33, 45, 45, 61, 71, 72, 73};
-        int target = 33;
-        System.out.println(binarySearchA(arr, target));
+        int[] arr = new int[] {141, 1, 17, -7, -17, -27, 18, 541, 8, 7, 7};
+        System.out.println(Arrays.toString(findThreeLargestNumbers(arr)));
 
     }
     /* TWO NUMBER SUM
@@ -401,6 +400,34 @@ public class Main {
             return binarySearchB(array, target, middle + 1, right);
         } else {
             return -1;
+        }
+    }
+    /* Find three largest numbers (do not sort array)
+       Solution # 1 O(n) time | O(1) space
+    */
+    public static int[] findThreeLargestNumbers(int[] array) {
+        int[] threeLargest = {Integer.MIN_VALUE, Integer.MIN_VALUE, Integer.MIN_VALUE};
+        for (int num : array) {
+            updateLargest(threeLargest, num);
+        }
+        return threeLargest;
+    }
+    public static void updateLargest(int[] threeLargest, int num) {
+        if (threeLargest[2] == 0 || num > threeLargest[2]) {
+            shiftAndUpdate(threeLargest, num, 2);
+        } else if (num > threeLargest[1]) {
+            shiftAndUpdate(threeLargest, num, 1);
+        } else if (num > threeLargest[0]) {
+            shiftAndUpdate(threeLargest, num, 0);
+        }
+    }
+    public static void shiftAndUpdate(int[] array, int num, int idx) {
+        for (int i=0; i <= idx; i++) {
+            if (i == idx) {
+                array[i] = num;
+            } else {
+                array[i] = array[i + 1];
+            }
         }
     }
 }
