833a53147bb068f318de615c24b6d96799d0cf22
Michael Botelho <saggiatoreblue@gmail.com>
2020-11-26 00:48:23 -0500

    bubble sort

--- src/com/michaelbotelho/Main.java
+++ src/com/michaelbotelho/Main.java
@@ -430,4 +430,30 @@ public class Main {
             }
         }
     }
+    /* Bubble Sort
+           Solution # 1 O(n^2) time | O(1) space
+    */
+    public static int[] bubbleSort(int[] array) {
+        if (array.length == 0)
+            return new int[] {};
+        boolean isSorted = false;
+        int counter = 0;
+        while (!isSorted) {
+            isSorted = true;
+            for (int i =0; i< array.length - 1 - counter; i++) {
+                if (array[i] > array[i + 1]) {
+                    swap(i, i+1, array);
+                    isSorted  = false;
+                }
+            }
+            counter++;
+        }
+        return array;
+    }
+
+    public static void swap(int i, int j, int[] array) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
 }
